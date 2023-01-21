c794a8eb7b8b4158b55f11f6c5dc83a0ecb754df
Muhammad Hafiz Azhari <80326383+mhafizazh@users.noreply.github.com>
2023-01-20 19:03:43 -0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+/*The calculator will be able to:
+
+- Add two integers
+- Subtract two integers
+- Multiply two integers
+- Divide two integers
+- Apply the modulo operator on two integers
+
+
+ */
+public class Calculator {
+
+    public Calculator(){
+
+    }
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b){
+        return a/b;
+    }
+    public int modulo(int a, int b){
+        return a % b;
+    }
+    public static void main(String[]args){
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5, 7));
+        System.out.println(calculator.subtract(45, 11));
+
+    }
+
+}
